56344494358cded46667511e2d4f89958716a7f8
BK <bhavesh.khatri@gmail.com>
2017-03-30 17:22:09 -0500

    Added Redis Cache for deployment and scaling purposes.

--- deployment/Usf.AzureResourceGroup/Usf.AzureResourceGroup/scripts/manager.ps1
+++ deployment/Usf.AzureResourceGroup/Usf.AzureResourceGroup/scripts/manager.ps1
@@ -1,5 +1,7 @@
 Param(
-    [Parameter()] [boolean]$Full = $false
+    [Parameter(Mandatory=$true)] [boolean]$Full = $false,
+    [Parameter(Mandatory=$true)] [string]$RedisHost = '',
+    [Parameter(Mandatory=$true)] [string]$RedisPassword = ''
 )
 
 $erroractionpreference = "Stop"
@@ -8,6 +10,12 @@ Write-Host "Manager node says: 'Hello World!'"
 if($Full -eq $True){
     Install-Module -Name Azure -Force -AllowClobber
     Install-Package -Name docker -ProviderName DockerMsftProvider -Verbose -Update -Force
+    Install-Package RespClient -Source "http://packages.nuget.org/v1/FeedService.svc/" -Verbose -Force
+    Import-Module "C:\Program Files\PackageManagement\NuGet\Packages\RespClient.1.1.13\content\RespClient.dll"
+    #Remove-Module RespClient
+    #Install-Package StackExchange.Redis -Source "http://packages.nuget.org/v1/FeedService.svc/" -Verbose -Force
+    #Import-Module "C:\Program Files\PackageManagement\NuGet\Packages\StackExchange.Redis.1.2.1\lib\net45\StackExchange.Redis.dll"
+    #Remove-Module StackExchange.Redis
 }
 
 Stop-Service docker -Verbose
@@ -33,3 +41,9 @@ Write-Host 'Manager Join Token:  ' $managerJoinToken
 Write-Host 'Worker Join Token:   ' $workerJoinToken
 Write-Host 'Manager IP:          ' $ipAddress
 Write-Host 'Manager Port:        ' $port
+
+Connect-RedisServer -Host $RedisHost -Port 6379
+Send-RedisCommand "auth " + $RedisPassword
+Send-RedisCommand "set managerJoinToken " + $managerJoinToken
+Send-RedisCommand "set workerJoinToken " + $workerJoinToken
+Disconnect-RedisServer
--- deployment/Usf.AzureResourceGroup/Usf.AzureResourceGroup/scripts/worker.ps1
+++ deployment/Usf.AzureResourceGroup/Usf.AzureResourceGroup/scripts/worker.ps1
@@ -1,3 +1,16 @@
+Param(
+    [Parameter()] [boolean]$Full = $false
+)
+
+$erroractionpreference = "Stop"
+
 Write-Host "Worker node says: 'Hello World!'"
-Install-Package -Name docker -ProviderName DockerMsftProvider -Verbose -Update
-Restart-Service docker
+if($Full -eq $True){
+    Install-Module -Name Azure -Force -AllowClobber
+    Install-Package -Name docker -ProviderName DockerMsftProvider -Verbose -Update -Force
+}
+
+Stop-Service docker -Verbose
+Start-Service docker -Verbose
+
+#docker swarm join --token SWMTKN-1-3sw3so7ki4kzbk5a0ep4idbnpsacyjig7sgd9iih9lkgpq0d4z-9p1fzc0a2gjz0zd428ffnqktc 10.0.0.6:2377
